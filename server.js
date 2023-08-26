74d78ad54b28b4dc099070949973fec03303467a
Miles Davis <foldes@mail.com>
2023-08-26 23:08:41 +0200

    cors

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+const { findLinkByShortened } = require('./dal/links')
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const PORT = 3001;
 const shortenerRoutes =  require('./routes/shortener');
 
+
+app.use(cors()); //use cors before we set up routes.
+
 app.use(express.json());
 app.use('/shortener', shortenerRoutes);
+app.get('/:shortened', async (req, res) => {
+    const link = await findLinkByShortened(req.params.shortened);
+
+    if (link) {
+        res.redirect(link.original);
+    } else {
+        res.status(404).send('Link is not available.');
+    }
+});
 
 //MongoDB:
 mongoose.connect('mongodb://localhost:27017/linkShortener', {
@@ -17,6 +31,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function() {
     console.log('Connected to MongoDB')
 })
+
+//cors:
+const corsOption ={
+    origin: 'http://localhost:3000',
+}
+app.use(cors(corsOption));
+
 
 app.get('/', (req, res) => {
     res.send('Sup!');

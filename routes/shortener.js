52271a11800537e558be611d92d72e0ba180ca2d
Miles Davis <foldes@mail.com>
2023-08-26 13:49:02 +0200

    base setup

---
+++ routes/shortener.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const { createLink, findLinkByShortened } = require('../dal/links');
+const shortid = require('shortid');
+
+
+router.post('/shorten', async (req, res) => {
+    const { original } = req.body;
+    const shortened = shortid.generate();
+    await findLinkByShortened.create({ original, shortened });
+    res.json({ original, shortened });
+});
+
+router.get('/:shortened', async ( req, res ) => {
+        const link = await findLinkByShortened(req.params.shortened);
+
+        if (link) {
+            res.redirect(link.original);
+        } else {
+            res.status(404).send()
+        }
+})

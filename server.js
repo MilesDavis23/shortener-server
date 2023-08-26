27e691437096280e54e8da70b4ce1518d8c184de
Miles Davis <foldes@mail.com>
2023-08-26 15:00:26 +0200

    mongoDB update

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 3001;
 const shortenerRoutes =  require('./routes/shortener');
 
 app.use(express.json());
 app.use('/shortener', shortenerRoutes);
+
+//MongoDB:
+mongoose.connect('mongodb://localhost:27017/linkShortener', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+    console.log('Connected to MongoDB')
+})
 
 app.get('/', (req, res) => {
     res.send('Sup!');

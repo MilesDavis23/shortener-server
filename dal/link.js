75a1c9d52231a7dade581de086dfeabff968bb85
Miles Davis <foldes@mail.com>
2023-08-26 13:07:17 +0200

    folders setup

---
+++ dal/link.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const LinkSchema = new mongoose.Schema({
+    original: String, 
+    shortened: String
+});
+
+const Link = mongoose.model('Link', LinkSchema);
+
+const createLink = async (original, shortened) =>{
+    return await Link.create({ original, shortened });
+};
+
+
+const findLinkByShortened = async (shortened) => {
+    return await Link.findOne({ shortened });
+};
+
+module.exports = {
+    createLink, 
+    findLinkByShortened
+};
